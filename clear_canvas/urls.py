8ee99f0fb88848e9e2af75cd3e8cce21de6c4dac
opu-hossain <opu.hossain.data@gmail.com>
2024-07-04 13:18:03 +0600

    Django initialized-1

--- clear_canvas/urls.py
+++ clear_canvas/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('canvas_ui.urls'))
-]
+    path('', include('canvas_core.urls')),
+    path('ui/', include('canvas_ui.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
